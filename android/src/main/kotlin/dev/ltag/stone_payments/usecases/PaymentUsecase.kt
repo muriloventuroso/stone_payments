81120eafc4c77748ecffdde7cafa394c6d04ac85
Murilo Venturoso <muriloventuroso@gmail.com>
2024-09-24 10:30:36 -0300

    Update

--- android/src/main/kotlin/dev/ltag/stone_payments/usecases/PaymentUsecase.kt
+++ android/src/main/kotlin/dev/ltag/stone_payments/usecases/PaymentUsecase.kt
@@ -265,30 +265,30 @@ class PaymentUsecase(
 
     private fun transactionToJson(message: TransactionObject) : String {
         var jsonString = "{";
-        jsonString.plus("acquirerTransactionKey: \"${message.acquirerTransactionKey}\",");
-        jsonString.plus("initiatorTransactionKey: \"${message.initiatorTransactionKey}\",");
-        jsonString.plus("amount: \"${message.amount}\",");
-        jsonString.plus("typeOfTransaction: \"${message.typeOfTransaction.name}\",");
-        jsonString.plus("instalmentTransaction: \"${message.instalmentTransaction.name}\",");
-        jsonString.plus("instalmentType: \"${message.instalmentType.name}\",");
-        jsonString.plus("cardHolderNumber: \"${message.cardHolderNumber}\",");
-        jsonString.plus("cardBrandName: \"${message.cardBrandName}\",");
-        jsonString.plus("cardHolderName: \"${message.cardHolderName}\",");
-        jsonString.plus("authorizationCode: \"${message.authorizationCode}\",");
-        jsonString.plus("transactionStatus: \"${message.transactionStatus.name}\",");
-        jsonString.plus("date: \"${message.date}\",");
-        jsonString.plus("time: \"${message.time}\",");
-        jsonString.plus("entryMode: \"${message.entryMode.toString()}\",");
-        jsonString.plus("aid: \"${message.aid}\",");
-        jsonString.plus("arcq: \"${message.arcq}\",");
-        jsonString.plus("shortName: \"${message.shortName}\",");
-        jsonString.plus("userModel: \"${message.userModel.toString()}\",");
-        jsonString.plus("pinpadUsed: \"${message.pinpadUsed}\",");
-        jsonString.plus("balance: \"${message.balance}\",");
-        jsonString.plus("isCapture: \"${message.isCapture.toString()}\",");
-        jsonString.plus("subMerchantCategoryCode: \"${message.subMerchantCategoryCode}\",");
-        jsonString.plus("subMerchantAddress: \"${message.subMerchantAddress}\",");
-        jsonString.plus("}");
+        jsonString = jsonString.plus("acquirerTransactionKey: \"${message.acquirerTransactionKey}\",");
+        jsonString = jsonString.plus("initiatorTransactionKey: \"${message.initiatorTransactionKey}\",");
+        jsonString = jsonString.plus("amount: \"${message.amount}\",");
+        jsonString = jsonString.plus("typeOfTransaction: \"${message.typeOfTransaction.name}\",");
+        jsonString = jsonString.plus("instalmentTransaction: \"${message.instalmentTransaction.name}\",");
+        jsonString = jsonString.plus("instalmentType: \"${message.instalmentType.name}\",");
+        jsonString = jsonString.plus("cardHolderNumber: \"${message.cardHolderNumber}\",");
+        jsonString = jsonString.plus("cardBrandName: \"${message.cardBrandName}\",");
+        jsonString = jsonString.plus("cardHolderName: \"${message.cardHolderName}\",");
+        jsonString = jsonString.plus("authorizationCode: \"${message.authorizationCode}\",");
+        jsonString = jsonString.plus("transactionStatus: \"${message.transactionStatus.name}\",");
+        jsonString = jsonString.plus("date: \"${message.date}\",");
+        jsonString = jsonString.plus("time: \"${message.time}\",");
+        jsonString = jsonString.plus("entryMode: \"${message.entryMode.toString()}\",");
+        jsonString = jsonString.plus("aid: \"${message.aid}\",");
+        jsonString = jsonString.plus("arcq: \"${message.arcq}\",");
+        jsonString = jsonString.plus("shortName: \"${message.shortName}\",");
+        jsonString = jsonString.plus("userModel: \"${message.userModel.toString()}\",");
+        jsonString = jsonString.plus("pinpadUsed: \"${message.pinpadUsed}\",");
+        jsonString = jsonString.plus("balance: \"${message.balance}\",");
+        jsonString = jsonString.plus("isCapture: \"${message.isCapture.toString()}\",");
+        jsonString = jsonString.plus("subMerchantCategoryCode: \"${message.subMerchantCategoryCode}\",");
+        jsonString = jsonString.plus("subMerchantAddress: \"${message.subMerchantAddress}\",");
+        jsonString = jsonString.plus("}");
         
         return jsonString;
     }
